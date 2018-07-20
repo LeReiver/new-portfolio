3bb6001754e3fefc0e352d9717888fa002aa17bd
Michael Le-Reiver <m.lereiver@gmail.com>
2018-07-20 15:31:28 -0700

    adding main elements

--- src/components/main/main.js
+++ src/components/main/main.js
@@ -5,7 +5,9 @@ export default class Main extends Component {
   render() {
     return (
       <main>
-          <h1 className="main-title">Full Stack Developer</h1>
+          <h1 className="main-title">Hello, I am a Full Stack Developer</h1>
+          <p>Experience with Full Stack Web Development using JavaScript, React, Redux, Node, PHP, Java, HTML5, &amp; CSS3. Currently in Thinkful's Engineering Immersion Program</p>
+          <button className="projects-button">View Projects</button>
       </main>
     );
   }
