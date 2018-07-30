ae52635e90fc5260b4a41031aa332bc1d6bae3e8
Michael Le-Reiver <m.lereiver@gmail.com>
2018-07-29 20:48:30 -0700

    Added project content, adjusted project element spacing and size for small devices

--- src/components/projects/projects.js
+++ src/components/projects/projects.js
@@ -14,10 +14,13 @@ render() {
         </div>
         <div className="project-desc">
           <h2>Vinylog</h2>
-          <p>Vinylog is web application that allows users to create a private and secure account to catalog their vinyl collection. By searching for an album title or artists name, users can retrieve a list of album release data including title, album image, genre, and release year. Users will be able to save results to their collection, view their collection, and remove them as well.</p>
-          <p className="p2"> JavaScript <span id="pipe">|</span> React/Redux <span id="pipe">|</span> Node <span id="pipe">|</span> Database Build <span id="pipe">|</span> JWT Auth <span id="pipe">|</span> Mocha/Chai Testing</p>
+          <p className="p2 desc">Vinylog is web application that allows users to create a private and secure account to catalog their vinyl collection. By searching for an album title or artists name, users can retrieve a list of album release data including title, album image, genre, and release year. Users will be able to save results to their collection, view their collection, and remove them as well.</p>
+          <p className="p2 stack">
+          Stack:
+            <blockquote>JavaScript, React/Redux, Node.js, MongoDB, JWT Auth, Mocha/Chai, HTML5, CSS3</blockquote>
+          </p>
             <div className="project-button">
-                <a href="https://vinylog.herokuapp.com/">
+                <a href="https://vinylog.herokuapp.com/" target="_blank">
                   <button>View Project</button></a>
             </div>
             <div className="repo-button">
@@ -25,25 +28,20 @@ render() {
                 <button>View Repo</button></a>
             </div>
         </div>
-      </div>
+      </div> 
 
       <div className="project-block">
-        <div className="project-image">
+        <div className="project-image ccs">
           <img src={require("../../images/ccs_desktop_md.png")}alt="CCS"></img>
         </div>
         <div className="project-desc">
           <h2>Coordinated Consulting Services</h2>
-          <p>A private and secure web application for a consulting firm built with PHP, JavaScript, and MySQL that
+          <p className="p2 desc">A private and secure web application for a consulting firm that
               requires user account creation and access, user input and uploads data to remote database with report generation capabilities.</p>
-          <p className="p2">Web Application Development <span id="pipe">|</span> Web Design <span id="pipe">|</span> Web Consulting <span id="pipe">|</span> Database Build</p>
-            <div className="project-button">
-                <a href="https://play.google.com/store/apps/details?id=app.lereiver.mike.drums" target="_blank" rel="noopener noreferrer">
-                  <button>View Project</button></a>
-            </div>
-            <div className="repo-button">
-                <a href="/../web/buddha_bellies/" target="_blank">
-                <button>View Repo</button></a>
-            </div>
+          <p className="p2 stack">
+          Stack:
+            <blockquote>PHP, JavaScript, MySQL, HTML5, CSS3</blockquote>
+          </p>
         </div>
       </div>
 
@@ -53,94 +51,22 @@ render() {
           </div>
           <div className="project-desc">
             <h2>Drums</h2>
-            <p>A rudimentary drum machine with language support for Italian and Japanese.</p>
-            <p className="p2">Android App Development <span id="pipe">|</span> App Design </p>
+            <p className="p2 desc">An Android app that I built for fun. It is a rudimentary drum machine with language support for Italian and Japanese.</p>
+          <p className="p2 stack">
+          Stack:
+            <blockquote>Java, XML, CSS3, Gradle, Android</blockquote>
+          </p>
             <div className="project-button">
                 <a href="https://play.google.com/store/apps/details?id=app.lereiver.mike.drums" target="_blank" rel="noopener noreferrer">
                   <button>View Project</button></a>
             </div>
             <div className="repo-button">
-                <a href="https://play.google.com/store/apps/details?id=app.lereiver.mike.drums" target="_blank" rel="noopener noreferrer">
+                <a href="https://github.com/LeReiver/drums" target="_blank" rel="noopener noreferrer">
                   <button>View Repo</button></a>
             </div>
           </div>
       </div>
 
-      <div className="project-block">
-        <div className="project-image">
-            <img src={require("../../images/guano_desktop_phone.png")} alt="Guano" title="Click To View"/>
-        </div>
-        <div className="project-desc">
-            <h2>Guano</h2>
-            <p>A mobile-first website for a band, built with PHP and jQuery and a responsive CSS grid utilizing LESS. Incorporated a modifiable Audio Controller Plugin as an audio player.</p>
-            <p className="p2">Web Development <span id="pipe">|</span> Web Design </p>
-            <div className="project-button">
-                <a href="https://play.google.com/store/apps/details?id=app.lereiver.mike.drums" target="_blank" rel="noopener noreferrer">
-                  <button>View Project</button></a>
-            </div>
-            <div className="repo-button">
-                <a href="/../web/guano/" target="_blank" rel="noopener noreferrer">
-                    <button>View Repo</button></a>
-            </div>
-        </div>
-      </div>
-
-      <div className="project-block">
-        <div className="project-image">
-            <img src={require("../../images/burrito_desktop_iphone.png")} alt="Burrito Masala" title="Click To View"/>
-        </div>
-        <div className="project-desc">
-            <h2>Burrito Masala</h2>
-            <p>This fictitious food cart mobile-first website was built with PHP, utilizing responsive CSS and LESS.</p>
-            <p className="p2">Web Development <span id="pipe">|</span> Web Design </p>
-            <div className="project-button">
-                <a href="https://play.google.com/store/apps/details?id=app.lereiver.mike.drums" target="_blank" rel="noopener noreferrer">
-                  <button>View Project</button></a>
-            </div>
-            <div className="repo-button">
-                <a href="/../web/burrito_masala/" target="_blank" rel="noopener noreferrer">
-                    <button >View Repo</button></a>
-            </div>
-        </div>
-      </div>
-
-      <div className="project-block">
-        <div className="project-image">
-                <img src={require("../../images/tip_calculator_desktop.png")} alt="Tip Calculator" title="Click To View" />
-        </div>
-        <div className="project-desc">
-            <h2>A Tip Calculator</h2>
-            <p>A Single Page Web Application built with PHP, HTML5 and CSS3.</p>
-            <p className="p2">Web Application Development <span id="pipe">|</span> Web Application Design </p>
-            <div className="project-button">
-                <a href="https://play.google.com/store/apps/details?id=app.lereiver.mike.drums" target="_blank" rel="noopener noreferrer">
-                  <button>View Project</button></a>
-            </div>
-            <div className="repo-button">
-                <a href="/../php/tip_calculator" target="_blank">
-                    <button>View Repo</button></a>
-            </div>
-        </div>
-      </div>
-
-      <div className="project-block">
-        <div className="project-image">
-            <img src={require("../../images/buddhabellies_desktop.png")} alt="Buddha Bellies Noodle House" title="Click To View" />
-        </div>
-        <div className="project-desc">
-            <h2>Buddha Bellies Noodle House</h2>
-            <p>A fictitious restaurant website built with hand rolled HTML5 and CSS3.</p>
-            <p className="p2">Web Development <span id="pipe">|</span> Web Design </p>
-            <div className="project-button">
-                <a href="https://play.google.com/store/apps/details?id=app.lereiver.mike.drums" target="_blank" rel="noopener noreferrer">
-                  <button>View Project</button></a>
-            </div>
-            <div className="repo-button">
-                <a href="/../web/buddha_bellies/" target="_blank">
-                <button>View Repo</button></a>
-            </div>
-        </div>
-        </div>
     </div>
     );
   }
