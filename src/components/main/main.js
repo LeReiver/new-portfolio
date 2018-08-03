ef16cc4e0218c69bf490beb7d9f58d408cbce8bb
Michael Le-Reiver <m.lereiver@gmail.com>
2018-08-02 20:12:05 -0700

    modified heading statement

--- src/components/main/main.js
+++ src/components/main/main.js
@@ -8,7 +8,7 @@ export default class Main extends Component {
       <div id="home">
         <main>
           <h1 className="main-title">Hello,<br/>I am a Full Stack Web Developer.</h1>
-          <p className="p2 left">Creating digital solutions is my passion.</p>
+          <p className="p2 left">Creating digital media is my passion.</p>
           <Link to="projects"><button className="projects-button">View Projects</button></Link>
         </main>
       </div>
