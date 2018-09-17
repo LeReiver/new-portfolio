c6c7be725010ca7e4ec990bd4d1b667875b48dab
Michael Le-Reiver <m.lereiver@gmail.com>
2018-09-17 16:54:41 -0700

    minor updates to about page background image size

--- src/components/about/about.js
+++ src/components/about/about.js
@@ -4,7 +4,9 @@ import './about.css';
 export default class About extends Component {
   render() {
     return (
-      <section role="complementary" id="about">
+      <section role="complementary" id="about"
+        //  style ={ { backgroundImage: "url('../../images/hero4.jpg')"}}
+         >
         <h1 className="about-title">Driven by Curiosity, Technology, Sound, and Design</h1>
           <p className="p2 left about">Seeking a position where I can assist and contribute to a team of dynamic developers while strengthening my skills utilizing JavaScript, React, Redux, Node, HTML5, &amp; CSS3.
           </p>
