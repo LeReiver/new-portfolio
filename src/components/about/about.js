47928104fb3e5c8ca3745d1f2377aeec939e0f97
Michael Le-Reiver <m.lereiver@gmail.com>
2018-10-09 13:14:59 -0700

    updated About bio

--- src/components/about/about.js
+++ src/components/about/about.js
@@ -9,7 +9,7 @@ export default class About extends Component {
         <h1 className="about-title">Driven by Curiosity, Mastery, Technology, Sound, and Design</h1>
           <img className="bio-pic" src={require('../../images/bio-pic.jpg')}></img>
           <p className="p2 left about">
-          My experience with music, art, traveling, food, and most importantly people, has provided me with skills to be a successful developer. I am innately compelled to learn and share new things, whether its the latest JavaScript framework, new cooking techniques or yet another musical instrument. I enjoy challenges that make me think outside the box and require creative solutions as well as surrounding myself with diverse people who bring their own passion to the table.
+          My experience with music, art, food, travel, and most importantly people, has provided me with skills to be a successful developer. I am innately compelled to learn and share new things, whether its the latest JavaScript frameworks, new cooking techniques or yet another musical instrument. I enjoy challenges that make me think outside the box and require creative solutions as well as surrounding myself with diverse people who bring their own passion to the table.
           </p>
       </section>
     );
